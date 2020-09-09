f625ebf4d20e33deefe2a08bcd8edaae8b1d20e0
John Michael Serminio <john-michael.s1@trans-cosmos.co.jp>
2020-09-09 14:35:42 +0800

    Initial Commit

---
+++ converting_timestamp_to_time_ago_inPHP.php
@@ -0,0 +1,39 @@
+<?php 
+// This function will focus on displaying current time(5 minutes ago)
+
+$datetime = '2020-09-07 10:54:27'; // this string will be get by database(on update current_timestamp)
+
+    function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+
+// call the function time_epased_string and variable "datetime";
+echo time_elapsed_string($datetime);
+
+
+?>
